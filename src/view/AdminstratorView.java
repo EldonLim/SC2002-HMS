5cb7db4a608c74e718478638cb45ed9fe8fa16d9
Eldon <eldonlimkaijie114@gmail.com>
2024-11-01 11:55:09 +0800

    Refactored Administrator View

--- src/view/AdminstratorView.java
+++ src/view/AdminstratorView.java
@@ -36,104 +36,25 @@ public class AdminstratorView implements View{
 
         } while (choice < 1 || choice > 5);
 
-        String response;
         switch (choice) {
             case 1:
-                // filter out patient
-                ArrayList<User> staffs = new ArrayList<>();
-                for (Map.Entry<String, User> user: DataBase.Users.entrySet()){
-                    if (!(user.getValue().getRole() == Role.PATIENT)) {
-                        staffs.add(user.getValue());
-                    }
-                }
                 System.out.println("List of hospital staffs: ");
-                for (User staff: staffs){
-                    System.out.println("Staff Name: " + staff.getName() + ", Staff id: " + staff.getID());
-                }
-                System.out.println("Enter 1 to add staff, 2 to remove staff: ");
-                int res = Helper.readInt();
-                if (res == 1){
-                    System.out.println("Enter staff name: ");
-                    String name = Helper.readString();
-                    System.out.println("Enter staff id: ");
-                    String id = Helper.readString();
-                    System.out.println("Enter staff role (1 for Administrator, 2 for Pharmacist, 3 for Doctor): ");
-                    int roleInt = Helper.readInt();
-                    Role role;
-                    if (roleInt == 1) role = Role.ADMINISTRATOR;
-                    if (roleInt == 2) role = Role.PHARMACIST;
-                    else role = Role.DOCTOR;
-
-                    System.out.println("Enter staff gender (M/F): ");
-                    Character genderChar = Helper.readChar();
-                    Gender gender;
-                    if (genderChar.equals("M")) gender = Gender.MALE;
-                    else gender = Gender.FEMALE;
-
-                    System.out.println("Enter staff age: ");
-                    int age = Helper.readInt();
-
-                    AdministratorManager.addUser(name, id, "password", role, gender, age);
-                    System.out.println("Staff added!");
-                } else {
-                    System.out.println("Enter staff id: ");
-                    String staffId = Helper.readString();
-                    AdministratorManager.removeUser(staffId);
-                    System.out.println("Staff has been removed!");
-                }
-                staffs.clear();
-                for (Map.Entry<String, User> user: DataBase.Users.entrySet()){
-                    if (!(user.getValue().getRole() == Role.PATIENT)) {
-                        staffs.add(user.getValue());
-                    }
-                }
+                listStaffs();
+                addOrRemoveStaffs();
                 System.out.println("Updated list of hospital staffs: ");
-                for (User staff: staffs){
-                    System.out.println("Staff Name: " + staff.getName() + ", Staff id: " + staff.getID());
-                }
+                listStaffs();
                 break;
             case 2:
                 
                 break;
             case 3:
-                ArrayList<Medicine> medicineList = new ArrayList<Medicine>();
-                medicineList = InventoryManager.getMedicationList();
-                for (int i=0; i<medicineList.size(); i++) {
-                    System.out.println("Medicine: " + medicineList.get(i).getMedicineName());
-                    System.out.println("Quantity: " + medicineList.get(i).getStock());
-                }
-                do {
-                    System.out.println("Enter the name of medicine that you would like to replenish (Enter NA if not applicable): ");
-                    response = Helper.readString();
-                    int quantity;
-                    if (!response.equals("NA")){
-                        System.out.println("Enter order quantity: ");
-                        quantity = Helper.readInt();
-                        InventoryManager.updateStock(response, quantity);
-                    }
-                    
-                } while (!response.equals("NA"));
+                listMedicine();
+                replenishMedicine();
                 System.out.println("Updated medicine list: ");
-                medicineList = InventoryManager.getMedicationList();
-                for (int i=0; i<medicineList.size(); i++) {
-                    System.out.println("Medicine: " + medicineList.get(i).getMedicineName());
-                    System.out.println("Quantity: " + medicineList.get(i).getStock());
-                }
+                listMedicine();
                 break;
             case 4:
-                Medicine[] replenishmentList = InventoryManager.getRequestedAddStockList();
-                for (int i=0; i<replenishmentList.length; i++) {
-                    if (replenishmentList[i] == null) break;
-                    System.out.println("Medicine: " + replenishmentList[i].getMedicineName() + " Order Quantity: " + replenishmentList[i].getOrderQuantity());
-                }
-                for (int i=0; i<replenishmentList.length; i++){
-                    System.out.println("Do you approve to replenish " + replenishmentList[i].getMedicineName() + "?");
-                    response = Helper.readString();
-                    if (response == "y"){
-                        InventoryManager.updateStock(replenishmentList[i].getMedicineName(), replenishmentList[i].getOrderQuantity());
-                        InventoryManager.approvedReplenishment(replenishmentList[i].getMedicineName());
-                    }
-                }
+                viewReplenishmentRequest();
                 break;
             case 5:
 
@@ -146,4 +67,94 @@ public class AdminstratorView implements View{
 
     @Override
     public void viewTitle() { System.out.println("Adminstrator Menu"); }
+
+    public void listStaffs() {
+        // filter out patient
+        ArrayList<User> staffs = new ArrayList<>();
+        for (Map.Entry<String, User> user: DataBase.Users.entrySet()){
+            if (!(user.getValue().getRole() == Role.PATIENT)) {
+                staffs.add(user.getValue());
+            }
+        }
+        
+        for (User staff: staffs){
+            System.out.println("Staff Name: " + staff.getName() + ", Staff id: " + staff.getID());
+        }
+    }
+
+    public void addOrRemoveStaffs() {
+        System.out.println("Enter 1 to add staff, 2 to remove staff: ");
+        int res = Helper.readInt();
+        if (res == 1){
+            System.out.println("Enter staff name: ");
+            String name = Helper.readString();
+            System.out.println("Enter staff id: ");
+            String id = Helper.readString();
+            System.out.println("Enter staff role (1 for Administrator, 2 for Pharmacist, 3 for Doctor): ");
+            int roleInt = Helper.readInt();
+            Role role;
+            if (roleInt == 1) role = Role.ADMINISTRATOR;
+            if (roleInt == 2) role = Role.PHARMACIST;
+            else role = Role.DOCTOR;
+
+            System.out.println("Enter staff gender (M/F): ");
+            Character genderChar = Helper.readChar();
+            Gender gender;
+            if (genderChar.equals("M")) gender = Gender.MALE;
+            else gender = Gender.FEMALE;
+
+            System.out.println("Enter staff age: ");
+            int age = Helper.readInt();
+
+            AdministratorManager.addUser(name, id, "password", role, gender, age);
+            System.out.println("Staff added!");
+        } else {
+            System.out.println("Enter staff id: ");
+            String staffId = Helper.readString();
+            AdministratorManager.removeUser(staffId);
+            System.out.println("Staff has been removed!");
+        }
+    }
+    
+    public void listMedicine(){
+        ArrayList<Medicine> medicineList = new ArrayList<Medicine>();
+        medicineList = InventoryManager.getMedicationList();
+        for (int i=0; i<medicineList.size(); i++) {
+            System.out.println("Medicine: " + medicineList.get(i).getMedicineName());
+            System.out.println("Quantity: " + medicineList.get(i).getStock());
+        }
+    }
+
+    public void replenishMedicine() {
+        String response;
+        do {
+            System.out.println("Enter the name of medicine that you would like to replenish (Enter NA if not applicable): ");
+            response = Helper.readString();
+            int quantity;
+            if (!response.equals("NA")){
+                System.out.println("Enter order quantity: ");
+                quantity = Helper.readInt();
+                InventoryManager.updateStock(response, quantity);
+                System.out.println("Medicine replenished!");
+            }
+            
+        } while (!response.equals("NA"));
+    }
+
+    public void viewReplenishmentRequest() {
+        String response;
+        Medicine[] replenishmentList = InventoryManager.getRequestedAddStockList();
+        for (int i=0; i<replenishmentList.length; i++) {
+            if (replenishmentList[i] == null) break;
+            System.out.println("Medicine: " + replenishmentList[i].getMedicineName() + " Order Quantity: " + replenishmentList[i].getOrderQuantity());
+        }
+        for (int i=0; i<replenishmentList.length; i++){
+            System.out.println("Do you approve to replenish " + replenishmentList[i].getMedicineName() + "?");
+            response = Helper.readString();
+            if (response == "y"){
+                InventoryManager.updateStock(replenishmentList[i].getMedicineName(), replenishmentList[i].getOrderQuantity());
+                InventoryManager.approvedReplenishment(replenishmentList[i].getMedicineName());
+            }
+        }
+    }
 }
