b45af571b10d4aa2a551b2e4f14a8cb9e0a665a8
linnsheng <linnsheng2002@gmail.com>
2024-11-03 13:19:36 +0800

    added .jar file to run the program

--- src/HMSApp.java
+++ src/HMSApp.java
@@ -1,12 +1,9 @@
 import database.DataBase;
-import view.*;
+import view.HMSAppView;
 
 public class HMSApp {
     public static void main(String[] args) {
-
         new DataBase();
         HMSAppView hmsAppView = new HMSAppView();
     }
-
-
 }
