fe20d88956c0d0fffd4e1decbcc543794871a388
linnsheng <linnsheng2002@gmail.com>
2024-11-13 00:59:52 +0800

    Finish

---
+++ src/helper/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides utility classes and methods for the Hospital Management System (HMS),
+ * including input handling, password encryption and CSV data parsing.
+ *
+ * @since 2024-10-27
+ */
+package helper;
---
+++ src/main/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Main package for the Hospital Management System (HMS) application.
+ * This package contains the entry point class {@link main.HMSApp}, which initializes
+ * the database and launches the main application view.
+ *
+ * <p>The {@link main.HMSApp} class serves as the starting point for the HMS application.
+ * It creates an instance of {@link database.DataBase} to initialize the system's
+ * database and displays the main view through {@link view.HMSAppView}.
+ * </p>
+ *
+ * @see database
+ * @see view
+ *
+ * @since 2024-10-26
+ * @version 5.5
+ */
+package main;
